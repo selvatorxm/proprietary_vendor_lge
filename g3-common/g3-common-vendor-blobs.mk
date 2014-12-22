be80b8f741ac8dc6eff3ad9e25ba7dc318a6f51c
firebird11 <hbgassel@gmail.com>
2014-12-22 15:16:00 +0100

    g3-common - change sources
     Changes to be committed:
    	modified:   g3-common/g3-common-vendor-blobs.mk
    	modified:   g3-common/proprietary/app/TimeService.apk
    	modified:   g3-common/proprietary/app/com.qualcomm.location.apk
    	modified:   g3-common/proprietary/app/com.qualcomm.services.location.apk
    	modified:   g3-common/proprietary/app/qcrilmsgtunnel.apk
    	modified:   g3-common/proprietary/app/shutdownlistener.apk
    	modified:   g3-common/proprietary/bin/adsprpcd
    	modified:   g3-common/proprietary/bin/irsc_util
    	modified:   g3-common/proprietary/bin/mm-pp-daemon
    	modified:   g3-common/proprietary/bin/mm-qcamera-daemon
    	modified:   g3-common/proprietary/bin/mpdecision

--- g3-common/g3-common-vendor-blobs.mk
+++ g3-common/g3-common-vendor-blobs.mk
@@ -149,8 +149,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/g3-common/proprietary/etc/firmware/DLdata_rev28B_data2.ecl:system/etc/firmware/DLdata_rev28B_data2.ecl \
     vendor/lge/g3-common/proprietary/etc/firmware/DLdata_rev28B_data3.ecl:system/etc/firmware/DLdata_rev28B_data3.ecl \
     vendor/lge/g3-common/proprietary/bin/qseecomd:system/bin/qseecomd \
-    vendor/lge/g3-common/proprietary/lib/liboemcrypto.so:system/lib/liboemcrypto.so \
-    vendor/lge/g3-common/proprietary/lib/drm/libfwdlockengine.so:system/lib/drm/libfwdlockengine.so \
+    vendor/lge/g3-common/proprietary/lib/libWVphoneAPI.so:system/lib/libWVphoneAPI.so \
     vendor/lge/g3-common/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
     vendor/lge/g3-common/proprietary/vendor/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
     vendor/lge/g3-common/proprietary/vendor/lib/libdrmdiag.so:system/vendor/lib/libdrmdiag.so \
@@ -162,9 +161,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/g3-common/proprietary/vendor/lib/libStDrvInt.so:system/vendor/lib/libStDrvInt.so \
     vendor/lge/g3-common/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
     vendor/lge/g3-common/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
-    vendor/lge/g3-common/proprietary/vendor/lib/libWVphoneAPI.so:system/vendor/lib/libWVphoneAPI.so \
     vendor/lge/g3-common/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
-    vendor/lge/g3-common/proprietary/vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/vendor/lib/mediadrm/libdrmclearkeyplugin.so \
     vendor/lge/g3-common/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
     vendor/lge/g3-common/proprietary/etc/gps.conf:system/etc/gps.conf \
     vendor/lge/g3-common/proprietary/etc/izat.conf:system/etc/izat.conf \
@@ -213,6 +210,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/g3-common/proprietary/bin/irsc_util:system/bin/irsc_util \
     vendor/lge/g3-common/proprietary/etc/permissions/qcnvitems.xml:system/etc/permissions/qcnvitems.xml \
     vendor/lge/g3-common/proprietary/etc/permissions/qcrilhook.xml:system/etc/permissions/qcrilhook.xml \
+    vendor/lge/g3-common/proprietary/lib/liblgderp.so:system/lib/liblgderp.so \
     vendor/lge/g3-common/proprietary/bin/thermal-engine:system/bin/thermal-engine \
     vendor/lge/g3-common/proprietary/vendor/lib/libthermalclient.so:system/vendor/lib/libthermalclient.so \
     vendor/lge/g3-common/proprietary/vendor/lib/libthermalioctl.so:system/vendor/lib/libthermalioctl.so \
